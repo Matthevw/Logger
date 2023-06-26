263701c1b1935ec45e39944e339a360facbc5f84
Mateusz Sabała <mateusz.sabala@onet.pl>
2023-06-26 14:14:05 +0200

    Added README and comments

--- Logger/Logger.php
+++ Logger/Logger.php
@@ -5,11 +5,13 @@ class Logger extends \Monolog\Logger
 {
     public function info($message, array $context = [], object $executedClass = null): void
     {
-        // $this->addRecord(static::INFO, (string) $message, $context);
+        // Checks if the third argument is given
+        // Custom message is extended by path to the file
         if ($executedClass !== null){
             $message = $message . " PATH: " . get_class($executedClass);
         }
 
+        // Adds message to the log file
         $this->addRecord(static::INFO, (string) $message, $context);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Logger
+Logger module allows the user to log custom message to the custom logger file
+This exercise served to familiarise me with custom loggers, where to use them, and how to use them. 
+Custom loggers can be particularly useful for debugging. 
+
+## How to use Logger
+Use Logger module by using Dependency Injection
+
+## Where to find logger file
+Logger file path is: /var/log/m2m_custom_log.log
